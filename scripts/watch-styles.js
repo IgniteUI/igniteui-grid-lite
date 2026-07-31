5a8f902394a2f0585d80fd7a0284755df3696cee
Radoslav Karaivanov <rkaraivanov@infragistics.com>
2026-07-31 15:38:30 +0300

    build: Bump dependencies and unpin npm version (#74)

--- scripts/watch-styles.js
+++ scripts/watch-styles.js
@@ -1,37 +1,37 @@
+import { watch } from 'node:fs';
 import { writeFile } from 'node:fs/promises';
-import watch from 'node-watch';
+import { join } from 'node:path';
 import * as sass from 'sass-embedded';
 import { compileSass, SCSS_EXT, TEMPLATE, TEMPLATE_REG_EXP } from './sass.js';
 
-const watchOptions = {
-  recursive: true,
-  filter: (path) => {
-    return /.(?:scss)$/.test(path);
-  },
-};
-
 let updating = false;
 const compiler = await sass.initAsyncCompiler();
+const filter = (fileName) => /\.scss$/.test(fileName);
+const now = () => `[${new Date().toLocaleTimeString()}]`;
+// biome-ignore lint/suspicious/noConsole: Build info
+const report = (message) => console.info(`${now()} ${message}`);
+// biome-ignore lint/suspicious/noConsole: Build info
+const error = (message) => console.error(`${now()} ${message}`);
 
-const watcher = watch(['src/styles'], watchOptions, async (_, path) => {
-  if (updating) {
-    return;
-  }
+watch('src/styles', { recursive: true }, async (_, fileName) => {
+  if (!fileName || !filter(fileName) || updating) return;
 
+  const filePath = join('src/styles', fileName);
+  report(`${filePath} changed, updating...`);
   updating = true;
 
   try {
     await writeFile(
-      path.replace(SCSS_EXT, '.css.ts'),
-      TEMPLATE.replace(TEMPLATE_REG_EXP, await compileSass(path, compiler)),
+      filePath.replace(SCSS_EXT, '.css.ts'),
+      TEMPLATE.replace(TEMPLATE_REG_EXP, await compileSass(filePath, compiler)),
       'utf-8'
     );
+    report(`${filePath} updated successfully.`);
   } catch (e) {
-    // biome-ignore lint/suspicious/noConsole: Build info
-    console.error(e);
+    error(`Error updating ${filePath}: ${e}`);
+  } finally {
+    updating = false;
   }
+}).on('close', () => compiler.dispose());
 
-  updating = false;
-});
-
-watcher.on('close', () => compiler.dispose());
+report(`${now()} Watching for changes in src/styles...`);
